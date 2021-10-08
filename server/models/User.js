21308ac3ec937a38edfb94f7e8b7b7a8da287f7a
Jemel Williams <jwilliams25400@yahoo.com>
2021-10-08 12:39:27 -0400

    saving

--- server/models/User.js
+++ server/models/User.js
@@ -19,10 +19,10 @@ const userSchema = new Schema({
     required: true,
     minlength: 5,
   },
-  thoughts: [
+  movie: [
     {
       type: Schema.Types.ObjectId,
-      ref: 'Thought',
+      ref: 'Movies',
     },
   ],
 });
