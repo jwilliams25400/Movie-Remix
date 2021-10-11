693c5b14fd758b0bf39f6b0caae7dfef4c9fbca5
Theo <trgrf1@gmail.com>
2021-10-11 11:25:57 -0400

    refactored movie model

--- server/seeders/seed.js
+++ server/seeders/seed.js
@@ -1,17 +1,17 @@
 const db = require('../config/connection');
-const { User, Movie } = require('../models');
+const { User, Movies } = require('../models');
 const userSeeds = require('./userSeeds.json');
 const movieSeeds = require('./movieSeeds.json');
 
 db.once('open', async () => {
   try {
-    await Movie.deleteMany({});
+    await Movies.deleteMany({});
     await User.deleteMany({});
 
     await User.create(userSeeds);
 
     for (let i = 0; i < movieSeeds.length; i++) {
-      const { _id, director } = await Movie.create(movieSeeds[i]);
+      const { _id, director } = await Movies.create(movieSeeds[i]);
       const user = await User.findOneAndUpdate(
         { username: director },
         {
