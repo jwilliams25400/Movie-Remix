c647566d836391e9c023ef1c06e6732656890e7d
Theo <trgrf1@gmail.com>
2021-10-12 13:45:46 -0400

    saving searchform

--- client/src/components/MovieDetails/index.js
+++ client/src/components/MovieDetails/index.js
@@ -4,16 +4,31 @@ import React from 'react';
 function MovieDetails(props) {
     return (
         <div>
-            <div className="text-center">
+            <div className="text-left">
                 <img
                     alt={props.title}
                     className="img-fluid"
                     src={props.src}
                     style={{ margin: '0 auto' }}
                 />
+
+                <button
+                    onClick={props.handleFormSubmit}
+                    className="btn btn-primary"
+                    type="sumbit"
+                >
+                Save Movie
+                </button>
+
                 <h3>Director(s): {props.director}</h3>
                 <h3>Genre: {props.genre}</h3>
                 <h3>Released: {props.released}</h3>
+                <h3>Rated: {props.rated}</h3>
+                <h3>Rating: {props.rating}</h3>
+                <h3>Plot: {props.plot}</h3>
+                <h3>Actors: {props.actors}</h3>
+
+                <h2>Trailer: {props.trailer}</h2>
             </div>
         </div>
     );
--- client/src/components/SearchBar/index.js
+++ client/src/components/SearchBar/index.js
@@ -1,42 +1,13 @@
-import React, { useState } from 'react';
+import React from 'react';
 
-// We take our props object and assign each property to it's own variable name.
-// In this case we only passed one prop, `onFormSubmit`
-function SearchBar({ onFormSubmit }) {
-  // Our state variable for the search term. Defaults to "microsoft/vscode".
-  const [term, setTerm] = useState('microsoft/vscode');
-
-  const sendTerm = (e) => {
-    e.preventDefault();
-
-    onFormSubmit(term);
-  };
-
+function SearchForm(props) {
   return (
-    <>
-      <h1>Movie Search</h1>
-      <span className="text-primary">
-        Search for a Movie!
-      </span>
-      <hr></hr>
+    <form>
       <div className="form-group">
-        <form className="form" onSubmit={sendTerm}>
-          <div className="field">
-            <label style={{ marginRight: '5px' }}>Movie Search</label>
-            <input
-              type="text"
-              value={term}
-              onChange={(e) => setTerm(e.target.value)}
-              placeholder="Batman"
-            />
-            <button className="btn" style={{ margin: '5px' }}>
-              Search
-            </button>
-          </div>
-        </form>
+        <label htmlFor="search">Search</label>
+        <input>
+        </input>
       </div>
-    </>
-  );
+    </form>
+  )
 }
-
-export default SearchBar;
