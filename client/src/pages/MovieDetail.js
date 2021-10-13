2a73bd3bfd2b64346d9e673fe574f9a9dec571ab
Theo <trgrf1@gmail.com>
2021-10-13 14:12:16 -0400

    saving

--- client/src/pages/MovieDetail.js
+++ client/src/pages/MovieDetail.js
@@ -1,34 +1,52 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import { Button } from 'react-bootstrap';
 import Auth from '../utils/auth';
-import DETAILAPI from '../utils/DETAILAPI';
-import YOUTUBEAPI from '../utils/YOUTUBEAPI';
-import {useMutation} from '@apollo/client';
-import {SAVE_MOVIE} from '../utils/mutations';
-import SearchedMovies, {SearchMovies} from './SearchedMovies/index';
+import { detailAPI } from '../utils/DETAILAPI';
+import { trailerAPI } from '../utils/YOUTUBEAPI';
+import { useMutation } from '@apollo/client';
+import { SAVE_MOVIE } from '../utils/mutations';
+import SearchedMovies, { searchMovies, searchInput } from './SearchedMovies/index';
 
-const [saveMovie, {error}] = useMutation(SAVE_MOVIE);
+const [saveMovie, { error }] = useMutation(SAVE_MOVIE);
 
 const handleSaveMovie = async (movieTitle) => {
     const movieToSave = SearchedMovies.find((movie) => movie.movieTitle === movieTitle);
 
     const token = Auth.loggedIn() ? Auth.getToken() : null;
 
-    if(!token) {
+    if (!token) {
         return false;
     }
 
     try {
         const { data } = await saveMovie({
-            variables: { movieData: { ...movieToSave}}
+            variables: { movieData: { ...movieToSave } }
         });
 
         setSavedMovieTitles([...setSavedMovieTitles, movieToSave.movieTitle]);
-    } catch(err) {
+    } catch (err) {
         console.error(err);
     }
 };
 
+try {
+    const response = await detailAPI();
+
+    if (!response.ok) {
+        throw new Error('something went wrong!')
+    }
+
+    const { items } = await response.json();
+
+    const movieData = items.map((movie) => ({
+        movieId: movie.id,
+        title: movie.title,
+        director: movie.director
+        
+    }))
+} catch (err) {
+
+}
 
 function MovieDetails(props) {
     return (
@@ -41,14 +59,14 @@ function MovieDetails(props) {
                     style={{ margin: '0 auto' }}
                 />
 
-                <Button
+                {/* <Button
                     disabled={savedBookIds?.some((savedBookId) => savedBookId === book.bookId)}
                     className='btn-block btn-info'
                     onClick={() => handleSaveMovie(book.bookId)}>
                     {savedBookIds?.some((savedBookId) => savedBookId === book.bookId)
                       ? 'This book has already been saved!'
                       : 'Save this Book!'}
-                </Button>
+                </Button> */}
 
                 <h3>Director(s): {props.director}</h3>
                 <h3>Genre: {props.genre}</h3>
--- client/src/utils/DETAILAPI.js
+++ client/src/utils/DETAILAPI.js
@@ -3,6 +3,6 @@ import axios from 'axios';
 const OMDBURL2 = -process.env.REACT_APP_DETAILURL;
 const OMDBAPIKEY = process.env.REACT_APP_OMDBKEY;
 
-export const searchAPI = (query) => {
+export const detailAPI = (query) => {
       return fetch(`${OMDBURL2}${query}${OMDBAPIKEY}`);
   };
