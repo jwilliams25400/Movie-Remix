50297b53435534c0f6dde6298591546ca3f88225
Jemel Williams <jwilliams25400@yahoo.com>
2021-10-13 13:56:42 -0400

    PosterAPI updates

--- client/src/pages/SearchedMovies/index.js
+++ client/src/pages/SearchedMovies/index.js
@@ -1,103 +1,80 @@
-import React, {useState, useEffect} from 'react';
-import search from '../../utils/POSTERAPI';
+import React, { useState, useEffect } from "react";
+import { searchAPI } from "../../utils/POSTERAPI";
 
 import Auth from "../../utils/auth";
-import { useMutation} from "@apollo/client";
-import { SAVE_MOVIE } from '../../utils/mutations';
+import { useMutation } from "@apollo/client";
+import { SAVE_MOVIE } from "../../utils/mutations";
 
- const SearchedMovies = () => {
+const SearchedMovies = () => {
+  const [searchedMovies, setSearchMovies] = useState([]);
 
-  const [searchedMovies,setSearchMovies ] = useState([]);
+  const [searchInput, setSearchInput] = useState("");
 
-  const [searchInput, setSearchInput ] = useState('');
-  
-  const [saveMovie, {error}] = useMutation(SAVE_MOVIE);
+  const [saveMovie, { error }] = useMutation(SAVE_MOVIE);
 
-  
-  
   const handleFormSubmit = async (event) => {
     event.preventDefault();
-    
-    if(!searchInput) {
-      return("Please enter a movie title")
+
+    if (!searchInput) {
+      return "Please enter a movie title";
     }
-    
-    try{
-      const response = await fetch(POSTERAPI(searchInput));
-      
+
+    try {
+      const response = await searchAPI(searchInput);
+
       if (!response) {
         throw new Error("unable to find movie");
       }
-      
-      const { item } = await response.json();
-      
+
+      const { items } = await response.json();
+
       const movieData = items.map((movie) => ({
         title: informatiomn.Search[i].Title,
-        poster: movies.Search[i].Poster
+        poster: movie.Search[i].Poster,
       }));
-      
+
       setSearchMovies(movieData);
       setSearchInput("");
     } catch (err) {
-      console.log("unable to load movies")
+      console.log("unable to load movies");
     }
-  };  
+  };
 
-  
-  // searchMovies = (query) => {
-  //   console.log(query);
-  //   search(query)
-  //   .then(res => this.setState({ result: res.data.data }))
-  //   .catch(err => console.log(err));
-  // };
-  
-  // handleInputChange = event => {
-  //   const value = event.target.value;
-  //   const name = event.target.name;
-  //   this.setState({
-  //     [name]: value
-  //   });
-  // };
-  // const handleSaveMovie = async (movieId) => [
-  //   const 
-  // ] 
+  const handleSaveMovie = async (movieId) => {
+    const moviesToSave = searchedMovies.find(
+      (book) => movie.movieTitle === movieTitle
+    );
 
+    const token = Auth.loggedIn() ? Auth.getToken() : null;
+    if (!token) {
+      return false;
+    }
+    try {
+      const { info } = await saveMovie({
+        variable: { movieData: { ...moviesToSave } },
+      });
+      console.log(info);
+    } catch (err) {
+      console.error(err);
+    }
+  };
 
-    
-
-
-
-
-
-    
-    function SearchedMovies () {
-      return (
-        <div className="search-movie container" >
-            <div className="row movie">
-              <div className="col s6 m7 l12">
-                
-                <div className="card-holder">
-                  <div className="title-holder">
-                    <h8>Title goes here</h8>
-                  </div>
-                  <div className="card-body">
-                  <div className="align-content-center img-display">
-                   
-                  </div>
-                </div>
+    return (
+      <div className="search-movie container">
+        <div className="row movie">
+          <div className="col s6 m7 l12">
+            <div className="card-holder">
+              <div className="title-holder">
+                <h8>Title goes here</h8>
+              </div>
+              <div className="card-body">
+                <div className="align-content-center img-display"></div>
               </div>
             </div>
-           </div>
+          </div>
         </div>
+      </div>
     );
-  }
-  
-  
 };
-  
 
-
-
-
-  
-  export default SearchedMovies;
+export default SearchedMovies;
