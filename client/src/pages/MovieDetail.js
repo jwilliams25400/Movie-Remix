9b0b490eb6e2f0ffaa8f436992c91942ab290a4c
Theo <trgrf1@gmail.com>
2021-10-14 14:55:53 -0400

    saving

--- client/src/pages/MovieDetail.js
+++ client/src/pages/MovieDetail.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { Jumbotron, Container, Col, Form, Button, Card, CardColumns } from 'react-bootstrap';
+import {Button, Card} from 'react-bootstrap';
 import Auth from '../utils/auth';
 import { detailAPI } from '../utils/DETAILAPI';
 import { trailerAPI } from '../utils/YOUTUBEAPI';
