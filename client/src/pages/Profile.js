e7151c4dd52d96d570c333bc6198fe39c3000a23
Jemel Williams <jwilliams25400@yahoo.com>
2021-10-16 15:38:01 -0400

    profile updates

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -95,10 +95,77 @@ const Profile = () => {
               flexGrow: "0",
               flexShrink: "0",
               alignItem: "normal",
-              justifyContent: "flex-start"
+              justifyContent: "flex-start",
             }}
           >
-            <div></div>
+            <div
+              style={{
+                position: "relative",
+                width: "25",
+                paddingLeft: "0",
+                paddingRight: "0",
+                height: "100vh",
+                boxSizing: "border-box",
+              }}
+            >
+              <div
+                className="chat-groups"
+                style={{
+                  maxHeight: "100vh",
+                  overflow: "hidden, scroll",
+                  borderRight: "1px solid",
+                  backgroundColor: "white",
+                }}
+              >
+                <div
+                  className="chat-container"
+                  style={{
+                    width: "100",
+                    height: "100",
+                    borderRadius: "0px 0px 24px 24px",
+                  }}
+                >
+                  <div>
+                    <div
+                      className="new-chat"
+                      style={{
+                        padding: "16px 14px",
+                        marginLeft: "0px",
+                      }}
+                    >
+                      <div>
+                        <div style={{ height: "0" }}>
+                          <div
+                            style={{
+                              fontWeight: "500",
+                              fontSize: "24px",
+                              position: "relative",
+                              top: "4px",
+                              width: "50",
+                            }}
+                          >
+                            My Groups
+                          </div>
+                        </div>
+
+                        <div style={{ maxWidth: "100", textAlign: "right" }}>
+                          <button
+                            id="new-chat"
+                            className="primary-btn"
+                            style={{ height: "36px", fontSize: "15px", cursor:"pointer",
+                          padding:"8px 16px",
+                        borderRadius:"33px", backgroundColor:"blue" }}
+                          ><span role="img" aria-label="plus">+</span></button>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+
+                  <div></div>
+                </div>
+              </div>
+            </div>
+
             <div className="sdfjosdfaoafoijjk"></div>
             <div className="sdfjosdfaoafoijjk"></div>
           </div>
